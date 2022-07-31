766b9c11e2672dd359ac8f1597486d07324bc062
fandevid <radenirfan378@gmail.com>
2022-07-31 12:37:06 +0700

    page add-product

--- js/add-products.js
+++ js/add-products.js
@@ -1,8 +1,74 @@
-const swiper1 = new Swiper(".swiper-special-offers", {
-  spaceBetween: 10,
-  autoplay: true,
-  pagination: {
-    el: ".special-offers-pagintaion",
-    clickable: true,
-  },
-});
+const formAddProduct = document.forms["form-add-product"];
+const inpName = document.querySelector("form input[name='name']");
+const inpPrice = document.querySelector("form input[name='price']");
+const inpCategory = document.querySelector("form select[name='category']");
+const inpDescription = document.querySelector("form textarea[name='description']");
+const inpImage = document.querySelector("form input[name='image']");
+let validName = false,
+  validPrice = false,
+  validCatgory = true,
+  validDescription = false,
+  validImage = false;
+function setError(elem, msg = "") {
+  elem.classList.remove("is-valid");
+  elem.classList.add("is-invalid");
+  try {
+    elem.nextElementSibling.innerHTML = msg;
+  } catch (error) {}
+}
+inpName.oninput = function () {
+  const value = this.value.trim();
+  validName = false;
+  if (value.length < 6) return setError(this, "min. 5 karakter");
+  validName = true;
+  this.classList.remove("is-invalid");
+  this.classList.add("is-valid");
+};
+inpPrice.oninput = function () {
+  const value = this.value.trim();
+  validPrice = false;
+  if (value < 1000) return setError(this, "min. 1000 rupiah");
+  validPrice = true;
+  this.classList.remove("is-invalid");
+  this.classList.add("is-valid");
+};
+inpCategory.onchange = function () {
+  validCatgory = false;
+  this.classList.add("is-valid");
+  validCatgory = true;
+};
+inpDescription.oninput = function () {
+  const value = this.value.trim();
+  validDescription = false;
+  if (value.length < 10) return setError(this, "min. 5 karakter");
+  validDescription = true;
+  this.classList.remove("is-invalid");
+  this.classList.add("is-valid");
+};
+inpImage.onchange = function () {
+  validImage = false;
+  const file = this.files[0];
+  console.log(file.size);
+  if (!(file.type === "image/png" || file.type === "image/jpeg" || file.type === "image/jpg")) return setError(this, "*Please choose .jpg/.png/.jpeg file!");
+  if (file.size > 1048576) return setError(this, "*File size must less than 1MB!");
+  validImage = true;
+  this.classList.remove("is-invalid");
+  this.classList.add("is-valid");
+};
+
+formAddProduct.onsubmit = function () {
+  if (!(validName && validImage && validPrice && validCatgory && validDescription)) {
+    swal({
+      icon: "error",
+      title: "Gagal!",
+      text: "Pastikan anda mengisi form dengan benar.",
+    });
+    return false;
+  }
+  swal({
+    icon: "success",
+    title: "Berhasil!",
+    text: "Produk berhasil diposting.",
+  });
+  return false;
+};
---
+++ js/products.js
@@ -0,0 +1,49 @@
+const swiper1 = new Swiper(".swiper-category", {
+  slidesPerView: "auto",
+  spaceBetween: 5,
+  freeMode: true,
+});
+const productsContainer = document.querySelector(".products");
+const btnShowMore = document.querySelector(".btn-show-more");
+let lastIndex = 0;
+function getProducts() {
+  btnShowMore.disabled = true;
+  if (lastIndex >= 100)
+    return swal({
+      title: "Tidak Ditemukan!",
+      text: "Semua produk telah ditampilkan.",
+      icon: "warning",
+    });
+  for (i = 0; i < 20; i++) {
+    const price = Math.round(Math.random() * 99 + 1);
+    const views = Math.floor(Math.random() * 1000);
+    const likes = Math.floor(Math.random() * views);
+    const a = document.createElement("a");
+    a.classList.add("product");
+    a.href = "detail.html";
+    a.innerHTML;
+    a.innerHTML = `
+            <img class="product-image" src="assets/img/albert einstein.jpg" loading="lazy" alt="Image Produk" />
+            <div class="product-body">
+              <h5 class="product-title text-truncate mb-0">Nama Produk Harusnya Tampil Disini</h5>
+              <div class="d-flex justify-content-between">
+                <div class="product-price">Rp.${price}.000</div>
+                <div class="product-star">
+                  <i class="far fa-heart"></i> <span>${likes}</span>
+                </div>
+              </div>
+            </div>
+          `;
+    productsContainer.appendChild(a);
+  }
+  lastIndex += 20;
+  btnShowMore.disabled = false;
+}
+
+// getProduct();
+// btnShowMore.onclick = getProducts;
+
+getProducts();
+btnShowMore.onclick = (e) => {
+  getProducts();
+};
